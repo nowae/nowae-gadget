b0748f9d3e4a9b2a23afe88eeccdda9fdef5ba50
Marco Giammarini <marco.giammarini@gruppofilippetti.it>
2018-11-27 10:03:31 +0100

    Added firmware project for Christmas Tree Ball

---
+++ 06-Firmware/ChristmasTreeBall_kl15_mcux/source/ChristmasTreeBall_kl15_mcux.c
@@ -0,0 +1,43 @@
+/*
+ * This file is part of the Christmas Tree Ball project.
+ *
+ * Copyright (C) 2018 NOWAE.IT
+ *
+ * Authors:
+ *  Marco Giammarini <m.giammarini@nowae.it>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+ 
+#include <stdio.h>
+#include "board.h"
+
+/*
+ * @brief   Application entry point.
+ */
+int main(void)
+{
+    volatile static int i = 0 ;
+
+    while (1)
+    {
+        i++ ;
+    }
+    return 0 ;
+}
