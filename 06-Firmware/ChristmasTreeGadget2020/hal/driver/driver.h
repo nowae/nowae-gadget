036794d1a8d68902f12ca806c63526361e81c050
Marco Giammarini <m.giammarini@warcomeb.it>
2020-12-03 00:09:58 +0100

    Started working on TCA9554 driver

---
+++ 06-Firmware/ChristmasTreeGadget2020/hal/driver/driver.h
@@ -0,0 +1,14 @@
+/*
+ * ChristmasTree - Gadget 2020
+ * Copyright (C) 2020 NOWAE <http://www.nowae.it>
+ *
+ * Author:
+ *   Marco Giammarini <m.giammarini@warcomeb.it>
+ */
+
+#ifndef DRIVER_H
+#define DRIVER_H
+
+#include "TCA9554/TCA9554.h"
+
+#endif // DRIVER_H
